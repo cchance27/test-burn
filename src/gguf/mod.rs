a6839308c9a88ed9420fc01a63191fdd6207f7b3
Chris Chance <cchance27@users.noreply.github.com>
2025-09-28 23:00:36 -0400

    Support heterogeneous GGUF tensors

--- src/gguf/mod.rs
+++ src/gguf/mod.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)]
+use bytemuck::try_cast_slice;
 use byteorder::{LittleEndian, ReadBytesExt};
+use half::{bf16, f16};
 use memmap2::Mmap;
 use std::collections::HashMap;
 use std::fs::File;
@@ -211,6 +213,14 @@ pub struct GGUTensorInfo {
     pub dimensions: Vec<u64>,
     pub data_type: GGUFDataType,
     pub offset: u64,
+}
+
+/// Borrowed GGUF tensor data interpreted according to its element type.
+pub enum GGUFRawTensor<'data> {
+    F32(&'data [f32]),
+    F16(&'data [f16]),
+    BF16(&'data [bf16]),
+    Bytes(&'data [u8], GGUFDataType),
 }
 
 #[derive(Debug)]
@@ -709,6 +719,32 @@ impl GGUFFile {
         }
 
         Ok(())
+    }
+}
+
+impl GGUTensorInfo {
+    /// Interpret the raw tensor buffer as a typed slice when possible.
+    pub fn into<'a>(&self, file: &'a GGUFFile) -> Result<GGUFRawTensor<'a>, GGUFError> {
+        let raw = file.get_tensor_data(self)?;
+
+        match self.data_type {
+            GGUFDataType::F32 => {
+                let casted = try_cast_slice(raw)
+                    .map_err(|_| GGUFError::InvalidTensorData(format!("Tensor '{}' is not valid f32 data", self.name)))?;
+                Ok(GGUFRawTensor::F32(casted))
+            }
+            GGUFDataType::F16 => {
+                let casted = try_cast_slice(raw)
+                    .map_err(|_| GGUFError::InvalidTensorData(format!("Tensor '{}' is not valid f16 data", self.name)))?;
+                Ok(GGUFRawTensor::F16(casted))
+            }
+            GGUFDataType::BF16 => {
+                let casted = try_cast_slice(raw)
+                    .map_err(|_| GGUFError::InvalidTensorData(format!("Tensor '{}' is not valid bf16 data", self.name)))?;
+                Ok(GGUFRawTensor::BF16(casted))
+            }
+            _ => Ok(GGUFRawTensor::Bytes(raw, self.data_type)),
+        }
     }
 }
 
